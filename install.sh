661673304744b34b3fb95f700e7c7561cdf62384
hidsh <hideaki.sh@gmail.com>
2020-11-11 08:39:43 +0900

    add .lesskey

--- install.sh
+++ install.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
+put_a() {
+    ln -s $PWD/$1 ~/$1  	# put_a hoge           : ./hoge -> ~/hoge
+}
+put_b() {
+    ln -s $PWD/$1 $2    	# put_b hoge /tmp/fuga : ./hoge -> /tmp/fuga
+}
 
-files=".vimrc"
-
-for x in $files; do
-	ln -s $PWD/$x ~/$x
-done
-
+#----------------------------
+put_a .vimrc
+put_a .lesskey
