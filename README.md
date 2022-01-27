facfc8210535eae6787c279c4860c0513a8028f3
hidsh <775213+hidsh@users.noreply.github.com>
2022-01-27 17:28:04 +0900

    add .gitconfig .zshrc-mymod

--- README.md
+++ README.md
@@ -1,9 +1,26 @@
 # mysetting
 my .zshrc, .vimrc, .lesskey, and so on...
 
+# files
+```
+$ tree -a -I '.git|.DS_Store'
+|-- .dircolors
+|-- .gitconfig (*)
+|-- .gitignore
+|-- .lesskey
+|-- .vimrc
+|-- .zshrc-mymod
+|-- README.md
+`-- install.sh
+```
+
 ## install
+
+Note (*): So `.gitconfig` is commented out, it will *NOT* be installed at default. check that line then uncoment it, then install as following:
+
 ```
 git clone git@github.com:hidsh/mysetting.git
 cd mysetting
 ./install.sh
 ```
+
--- install.sh
+++ install.sh
@@ -1,19 +1,16 @@
 #!/bin/sh
 put_a() {
-    ln -s $PWD/$1 ~/$1  	# put_a hoge           : ./hoge -> ~/hoge
+    ln -f -s $PWD/$1 ~/$1  	# put_a hoge           : ./hoge -> ~/hoge
 }
 put_b() {
-    ln -s $PWD/$1 $2    	# put_b hoge /tmp/fuga : ./hoge -> /tmp/fuga
+    ln -f -s $PWD/$1 $2    	# put_b hoge /tmp/fuga : ./hoge -> /tmp/fuga
 }
 
-
-
 #----------------------------
-# common
-put_a .vimrc
+put_a .dircolors
+#put_a .gitconfig
+put_a .gitignore
 put_a .lesskey
+put_a .vimrc
+put_b .zshrc-mymod ~/.zprezto/runcoms/zshrc
 
-# wsl
-if [ $WSL_DISTRO_NAME ]; then
- put_b .dircolors-wsl ~/.dircolors
-fi
